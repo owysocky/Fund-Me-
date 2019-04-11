f038b0cd8c74c86af525a88ce96922fe0a35baa4
Olha Wysocky and Yulia Shidlovskaya <olha@>
2019-04-11 10:22:35 -0700

    add model

---
+++ src/app/models/idea.model.ts
@@ -0,0 +1,5 @@
+export class Idea{
+  moneyRisen: number = 0;
+  
+  constructor(public name: string, public category: string, public userName: string, public description: string, public moneyExpected: number){}
+}
